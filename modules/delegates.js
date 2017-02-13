f65519a31ab9c68d16690d55fe1778239963b2f9
fix <fx.thoorens@gmail.com>
2017-02-13 16:40:29 +0100

    check if there is a last block stored

--- modules/delegates.js
+++ modules/delegates.js
@@ -254,7 +254,7 @@ __private.forge = function (cb) {
 	var currentSlot = slots.getSlotNumber();
 	// If we are supposed to forge now, be sure we got the very last block
 	var lastBlock = modules.blockchain.getLastBlock();
-	if (currentSlot === slots.getSlotNumber(lastBlock.timestamp)) {
+	if (!lastBlock || currentSlot === slots.getSlotNumber(lastBlock.timestamp)) {
 		err = 'Last block within same delegate slot';
 		return setImmediate(cb, err);
 	}
