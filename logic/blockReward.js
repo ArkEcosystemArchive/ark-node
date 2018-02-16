9f169611e8b6fcbceebb5e963d736b963fdcdacc
Brian Faust <faustbrian@users.noreply.github.com>
2018-02-16 10:37:23 +0200

    fix calcSupply method

--- logic/blockReward.js
+++ logic/blockReward.js
@@ -61,49 +61,7 @@ BlockReward.prototype.calcReward = function (height) {
 
 //
 BlockReward.prototype.calcSupply = function (height) {
-	height        = __private.parseHeight(height);
-	var milestone = this.calcMilestone(height);
-	var supply    = constants.totalAmount / Math.pow(10,8);
-	var rewards   = [];
-
-	var amount = 0, multiplier = 0;
-
-	for (var i = 0; i < this.milestones.length; i++) {
-		if (milestone >= i) {
-			multiplier = (this.milestones[i] / Math.pow(10,8));
-
-			if (height < this.rewardOffset) {
-				break; // Rewards not started yet
-			} else if (height < this.distance) {
-				amount = height % this.distance; // Measure this.distance thus far
-			} else {
-				amount = this.distance; // Assign completed milestone
-				height -= this.distance; // Deduct from total height
-
-				// After last milestone
-				if (height > 0 && i === this.milestones.length - 1) {
-					var postHeight = this.rewardOffset - 1;
-
-					if (height >= postHeight) {
-						amount += (height - postHeight);
-					} else {
-						amount += (postHeight - height);
-					}
-				}
-			}
-
-			rewards.push([amount, multiplier]);
-		} else {
-			break; // Milestone out of bounds
-		}
-	}
-
-	for (i = 0; i < rewards.length; i++) {
-		var reward = rewards[i];
-		supply += reward[0] * reward[1];
-	}
-
-	return supply * Math.pow(10,8);
+	return constants.totalAmount + (__private.parseHeight(height) - constants.rewards.offset) * 2;
 };
 
 // Export
