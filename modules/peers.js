ca4bd28def6aeb02f2d5034f4b13905ee1059d56
George Kushnir <admin@n4ru.it>
2018-06-13 20:59:51 -0400

    another typo!

--- modules/peers.js
+++ modules/peers.js
@@ -108,7 +108,7 @@ __private.updatePeersList = function (cb) {
 
 			var peers = res.body.peers
 				.filter(peer => peer.ip.substr(0,3) != "127") // exclude loopback addresses
-				.peers.sort((a, b) => 0.5 - Math.random()) // randomize the list to prevent malicious list crafting
+				.sort((a, b) => 0.5 - Math.random()) // randomize the list to prevent malicious list crafting
 				.slice(0, reach); // don't query everyone - that would be spammy
 
 			async.each(peers, function (peer, eachCb) {
