696b6f480e48bbfb7dcabcdf6731e483e8c17a06
Brian Faust <envoyer@pm.me>
2018-08-21 15:25:17 +0300

    process block after validation

--- modules/nodeManager.js
+++ modules/nodeManager.js
@@ -158,7 +158,8 @@ NodeManager.prototype.onBlocksReceived = function(blocks, peer, cb) {
 			block.totalAmount = parseInt(block.totalAmount);
 			block.totalFee = parseInt(block.totalFee);
 			block.verified = false;
-		  block.processed = false;
+			block.processed = false;
+			if (block.numberOfTransactions == 0) block.transactions = [];
       // looks like the last block pulled, let's broadcast it
 			block.broadcast = blocks.length == 1;
 
@@ -499,12 +500,12 @@ NodeManager.prototype.onBlockReceived = function(block, peer, cb) {
 					modules.blockchain.removeBlock(block);
 					return mSequence(err, block);
 				}
-				modules.blockchain.upsertBlock(block);
 				library.logger.debug("processing block with "+block.transactions.length+" transactions", block.height);
 				return library.bus.message('verifyBlock', block, function(err){
 					if(err){
 						library.logger.error("Error processing block at height", block.height);
-						modules.blockchain.removeBlock(block);
+					} else {
+						modules.blockchain.upsertBlock(block);
 					}
 					return mSequence(err, block);
 				});
