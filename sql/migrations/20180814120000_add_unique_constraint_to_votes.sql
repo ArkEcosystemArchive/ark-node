f971e00b81c985d23100a8c0fa6b3a3225f2a68d
dafty <dafty235@gmail.com>
2018-08-21 13:43:40 +0100

    converting new indexes to unique constraints

--- sql/migrations/20180814120000_add_unique_constraint_to_votes.sql
+++ sql/migrations/20180814120000_add_unique_constraint_to_votes.sql
@@ -1,5 +1,5 @@
 BEGIN;
 
-CREATE INDEX IF NOT EXISTS "votes_uniq" ON votes ("votes", "transactionId");
+CREATE UNIQUE INDEX IF NOT EXISTS "votes_uniq" ON votes ("votes", "transactionId");
 
 COMMIT;
--- sql/migrations/20180814130000_add_unique_constraint_to_delegates.sql
+++ sql/migrations/20180814130000_add_unique_constraint_to_delegates.sql
@@ -1,6 +1,6 @@
 BEGIN;
 
-CREATE INDEX IF NOT EXISTS "delegates_username_uniq" ON delegates ("username");
-CREATE INDEX IF NOT EXISTS "delegates_transactionId_uniq" ON delegates ("transactionId");
+CREATE UNIQUE INDEX IF NOT EXISTS "delegates_username_uniq" ON delegates ("username");
+CREATE UNIQUE INDEX IF NOT EXISTS "delegates_transactionId_uniq" ON delegates ("transactionId");
 
 COMMIT;
