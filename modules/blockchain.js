5d5491243f713ce74e1b60fc67d58b15c7641e76
George Kushnir <n4ru@users.noreply.github.com>
2018-10-09 13:39:04 -0400

    Merge changes.

--- modules/blockchain.js
+++ modules/blockchain.js
@@ -477,18 +477,18 @@ __private.timestampState = function (lastReceipt) {
 	var timeNow = new Date().getTime();
 	__private.lastReceipt.secondsAgo = Math.floor((timeNow -  __private.lastReceipt.date.getTime()) / 1000);
 	if(modules.delegates.isActiveDelegate()){
-		__private.lastReceipt.stale = __private.lastReceipt.secondsAgo > 10;
-		__private.lastReceipt.rebuild = __private.lastReceipt.secondsAgo > 60;
+		__private.lastReceipt.stale = __private.lastReceipt.secondsAgo > 60;
+		__private.lastReceipt.rebuild = __private.lastReceipt.secondsAgo > 300;
 	}
 
 	else if(modules.delegates.isForging()){
-		__private.lastReceipt.stale = __private.lastReceipt.secondsAgo > 30;
-		__private.lastReceipt.rebuild = __private.lastReceipt.secondsAgo > 100;
+		__private.lastReceipt.stale = __private.lastReceipt.secondsAgo > 60;
+		__private.lastReceipt.rebuild = __private.lastReceipt.secondsAgo > 300;
 	}
 
 	else {
-		__private.lastReceipt.stale = __private.lastReceipt.secondsAgo > 60;
-		__private.lastReceipt.rebuild = __private.lastReceipt.secondsAgo > 200;
+		__private.lastReceipt.stale = __private.lastReceipt.secondsAgo > 300;
+		__private.lastReceipt.rebuild = __private.lastReceipt.secondsAgo > 500;
 	}
 
 	if(__private.lastBlock.height < 52){
